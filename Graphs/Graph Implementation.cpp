9f515c62a626da34a1636e19aead1c84a3df7893
Dhruv Trehan <46952155+DhruvTrehan@users.noreply.github.com>
2020-10-15 22:03:17 +0530

    Update Graph Implementation.cpp
    Graph Implementation using List

--- Graphs/Graph Implementation.cpp
+++ Graphs/Graph Implementation.cpp
@@ -1,10 +1,41 @@
 #include<iostream>
 #include<list>
 using namespace std;
-class graph
+class Graph 
 {
-	int v;
-	list<int > *l;
-	public:
-		
+    int V;
+    list<int> *l;
+
+    public:
+        Graph(int V)
+        {
+            this->V = V;
+            l = new list<int>[V]; //Declaring array of list 
+        }
+        void addEdge(int x, int y)
+        {
+            l[x].push_back(y);
+            l[y].push_back(x);
+        }
+        void print()
+        {
+            for(int i = 0 ; i < V ; i++)
+            {
+                cout<<"Vertex"<<i<<"->";
+                for(int nbr:l[i])
+                {
+                    cout<<nbr<<" ";
+                }
+                cout<<endl;
+            }
+        }
 };
+int main()
+{
+    Graph g(4);  // pass vertices in constructor
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(2,3);
+    g.addEdge(1,2);
+    g.print();              // print();   
+}
